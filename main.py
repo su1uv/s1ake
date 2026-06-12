79183c6ff0a7429bcd0f60a477bdca1322aab8c2
EmiliodDev <ortizemilio.dev@gmail.com>
2026-06-11 23:00:12 -0600

    calculator sub-project test

--- main.py
+++ main.py
@@ -3,32 +3,38 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from google.genai.client import Client
-from google.genai.types import GenerateContentResponse
 
 load_dotenv()
 
 
 def main():
     api_key: str | None = os.environ.get("GEMINI_API_KEY")
-
     if api_key is None:
         raise RuntimeError("error loading api key")
 
     client: Client = genai.Client(api_key=api_key)
 
-    parser = argparse.ArgumentParser(description="chatbot")
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="chatbot")
     parser.add_argument("user_prompt", type=str, help="user prompt")
-    args = parser.parse_args()
+    parser.add_argument("--verbose", action="store_true", help="enable verbose output")
+    args: argparse.Namespace = parser.parse_args()
 
-    response: GenerateContentResponse = client.models.generate_content(
-        model="gemini-2.5-flash", contents=args.user_prompt
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    response: types.GenerateContentResponse = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
     )
     if response.usage_metadata is None:
         raise RuntimeError("api request went wrong")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print(response.text)
 
